5dcf26d268098f19a5545e9367d3764f45623291
karishmakaja2003 <159711635+karishmakaja2003@users.noreply.github.com>
2025-05-21 19:29:16 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,19 @@
+public abstract class User {
+protected String id;
+protected String name;
+
+public User(String id, String name) {
+    this.id = id;
+    this.name = name;
+}
+
+public String getId() {
+    return id;
+}
+
+public String getName() {
+    return name;
+}
+
+public abstract void showMenu();
+}
